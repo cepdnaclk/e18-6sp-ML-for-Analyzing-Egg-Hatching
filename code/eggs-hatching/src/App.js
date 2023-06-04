86300d6d9b6fe574f6bd05eb0172e9e337cd33a7
SathsaraniAththanayaka <e18030@eng.pdn.ac.lk>
2023-06-04 17:51:05 +0530

    updated

--- code/eggs-hatching/src/App.js
+++ code/eggs-hatching/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          6SP - ML for Analyzing Eggs Hatching 
+          6SP - ML for Analyzing Eggs Hatching updated
         </p>
         <a
           className="App-link"

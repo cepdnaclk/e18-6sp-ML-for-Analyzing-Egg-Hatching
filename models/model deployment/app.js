26bd5f57766e9d1a0577a39f7a07b770cab97e39
NethmiRanasinghe <e18282@eng.pdn.ac.lk>
2023-09-10 22:32:37 +0530

    add model deployment

---
+++ models/model deployment/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+function extractNumberFromHex(hexString) {
+    // Convert the hexadecimal values to ASCII characters
+    const asciiString = Buffer.from(hexString, 'hex').toString('ascii');
+  
+    // Use regular expressions to extract the numeric part (e.g., 3343.0)
+    const numericMatch = asciiString.match(/\d+\.\d+/);
+  
+    if (numericMatch) {
+      // Convert the matched string to a number
+      const numericValue = parseFloat(numericMatch[0]);
+      return numericValue;
+    } else {
+      // Return null or handle the case where no numeric part is found
+      return null;
+    }
+  }
+
+app.post('/predict', (req, res) => {
+    const inputData = req.body;
+  
+    const pythonProcess = spawn('python', ['predict.py', JSON.stringify(inputData)]);
+  
+    let prediction = 0;
+    let errorMessage = ''; 
+  
+    pythonProcess.stdout.on('data', (data) => {
+      prediction = extractNumberFromHex(data); 
+    });
+  
+    pythonProcess.stderr.on('data', (data) => {
+      errorMessage += data; 
+    });
+  
+    pythonProcess.on('close', (code) => {
+      console.log(`Python script exited with code ${code}`);
+  
+      if (code === 0) {
+        // If the Python script exits with code 0, it's considered a success
+        res.json({ prediction: prediction });
+      } else {
+        // If the Python script exits with a non-zero code, there was an error
+        console.error(`Python stderr: ${errorMessage}`);
+        res.status(500).json({ error: 'An error occurred while running the Python script.' });
+      }
+    });
+  });
+  
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ models/model deployment/predict.py
@@ -0,0 +1,30 @@
+import pickle
+import sys
+import json
+import numpy as np  
+
+# Load the decision tree model
+model_file = 'decision_tree_model.pkl'
+with open(model_file, 'rb') as model:
+    clf = pickle.load(model)
+
+input_data = json.loads(sys.argv[1])
+
+# Convert input data to a NumPy array
+input_array = np.array([[
+    input_data["Age"],
+    input_data["Mortality female"],
+    input_data["Mortality male"],
+    input_data["sex ratio"],
+    input_data["Total Eggs"],
+    input_data["Egg Weight"],
+    input_data["Feed female"],
+    input_data["Feed male"]
+]])
+
+
+# Make predictions
+predictions = clf.predict(input_array)
+
+# Convert predictions to a JSON string and print it
+print(json.dumps(predictions.tolist()))
